c4ed94e104a0b87cc6bcb5b5aaa0987e791ea71a
joel.aguilar <joel.aguilar@MEXL002020>
2020-08-31 13:03:39 -0500

    JPA Base RestServices Development Completed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(101, 'kreddy@stacksimplify.com', 'Kalyan', 'Reddy', 'admin', 'ssn01', 'kreddy');
+insert into user values(102, 'qwiser@stacksimplify.com', 'Greg',   'Wiser', 'admin', 'ssn02', 'gwiser');
+insert into user values(103, 'dmark@stacksimplify.com',  'David',  'Mark',  'admin', 'ssn03', 'dmark');
